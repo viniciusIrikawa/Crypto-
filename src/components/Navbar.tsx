1e30c1d9d463f61d7722d8ca74143b5aed74a776
Vinicius <viniciusiandrade@hotmail.com>
2024-05-19 00:38:12 -0300

    feat: link in navbar to dashboard page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,11 +2,14 @@ const Navbar = () => {
   return (
     <nav className='flex justify-between items-center bg-[#000000b0] py-8 px-20 text-white-low'>
         <h2 className='font-bold text-2xl'>Crypto <span className='text-crypto-purple text-2xl'>+</span></h2>
-        <ul className='flex text-sm cursor-pointer font-bold'>
+        <ul className='flex items-center text-sm cursor-pointer font-bold'>
           <li className='mx-3 uppercase'> Cryptocurrencies </li>
           <li className='mx-3 uppercase'> NFT's </li>
           <li className='mx-3 uppercase'> About </li>
           <li className='mx-3 uppercase'> Blog </li>
+          <li className='mx-3 uppercase bg-crypto-purple px-7 py-2 rounded-sm'> 
+            <a href="/dashboard/BTCUSDT"> Go Trade </a>
+          </li>
         </ul>
     </nav>
   )
