e026b0cd1b12094fce1b098a7ed704a1548f1a67
Vinicius <viniciusiandrade@hotmail.com>
2024-05-18 21:29:35 -0300

    feat: trading view chart in Dashboard page

--- src/routes/Dashboard.tsx
+++ src/routes/Dashboard.tsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import { useEffect, useRef, memo } from 'react';
 
-const Dashboard = () => {
+function TradingViewWidget() {
+  const container = useRef<HTMLDivElement | null>(null);
+
+  useEffect(
+    () => {
+      const script = document.createElement("script");
+      script.src = "https://s3.tradingview.com/external-embedding/embed-widget-advanced-chart.js";
+      script.type = "text/javascript";
+      script.async = true;
+      script.innerHTML = `
+        {
+          "width": "80%",
+          "height": "750",
+          "symbol": "BTCUSDT",
+          "interval": "D",
+          "timezone": "Etc/UTC",
+          "theme": "dark",
+          "style": "1",
+          "locale": "en",
+          "allow_symbol_change": true,
+          "calendar": false,
+          "support_host": "https://www.tradingview.com"
+        }`;
+      container.current?.appendChild(script);
+    },
+    []
+  );
+
   return (
-    <div className='text-white'>Dashboard page</div>
-  )
+    <div className="tradingview-widget-container" ref={container}>
+      <div className="tradingview-widget-container__widget"></div>
+      <div className="tradingview-widget-copyright"><a href="https://www.tradingview.com/" rel="noopener nofollow" target="_blank"><span className="blue-text">Track all markets on TradingView</span></a></div>
+    </div>
+  );
 }
 
-export default Dashboard
+export default memo(TradingViewWidget);
