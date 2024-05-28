b426c2ec74cd31ee8e3343e048be2c3b7e391eaf
Vinicius <viniciusiandrade@hotmail.com>
2024-05-28 16:49:18 -0300

    fix: adding values in key attribute

--- src/components/TimeInterval.tsx
+++ src/components/TimeInterval.tsx
@@ -17,15 +17,15 @@ const TimeInterval = ({ timeInterval, setTimeInterval }: Type) => {
                     value={timeInterval} 
                     className='bg-[#393939] text-white rounded-md px-5 py-2 cursor-pointer' 
                     onChange={(e) => handleChange(e)}>
-            <option value="1" key=""> 1m </option>
-            <option value="5" key=""> 5m </option>
-            <option value="15" key=""> 15m </option>
-            <option value="30" key=""> 30m </option>
-            <option value="60" key=""> 1h </option>
-            <option value="240" key=""> 4h </option>
-            <option value="D" key=""> 1D </option>
-            <option value="W" key=""> 1w </option>
-            <option value="M" key=""> 1M </option>
+            <option value="1" key="1"> 1m </option>
+            <option value="5" key="2"> 5m </option>
+            <option value="15" key="3"> 15m </option>
+            <option value="30" key="4"> 30m </option>
+            <option value="60" key="5"> 1h </option>
+            <option value="240" key="6"> 4h </option>
+            <option value="D" key="7"> 1D </option>
+            <option value="W" key="8"> 1w </option>
+            <option value="M" key="9"> 1M </option>
             </select>
         </div>
     )
