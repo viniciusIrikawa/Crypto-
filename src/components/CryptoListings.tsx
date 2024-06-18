fae8deea48784b2bee03132f5430c39b76fcbf8b
Vinicius <viniciusiandrade@hotmail.com>
2024-06-18 18:00:58 -0300

    testing3

--- src/components/CryptoListings.tsx
+++ src/components/CryptoListings.tsx
@@ -1,7 +1,7 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Table from "./Table";
 import { PriceData } from "../Types/price";
-import { initConnection } from "../services/websocket";
+// import { initConnection } from "../services/websocket";
 
 const CryptoListings = () => {
   const [prices, setPrices] = useState<{ [pair: string]: PriceData }>({});
@@ -10,6 +10,7 @@ const CryptoListings = () => {
   //   initConnection(setPrices);
   // }, []);
 
+  setPrices(null!)
   return (
       <Table prices={prices}/>
   );
